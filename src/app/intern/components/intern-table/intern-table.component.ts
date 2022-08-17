4ccea77d775d9438186ce64e1bb1ab6a7b57cf8c
pia <pia.andreotti@gmail.com>
2022-08-17 14:49:58 +0200

    UPDATE move logic from appComponent to  internTableComponent

--- src/app/intern/components/intern-table/intern-table.component.ts
+++ src/app/intern/components/intern-table/intern-table.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Logger } from './../../../core/helpers/logger';
+import { Intern } from './../../../core/models/intern';
 
 @Component({
   selector: 'app-intern-table',
@@ -11,5 +13,79 @@ export class InternTableComponent implements OnInit {
 
   ngOnInit(): void {
   }
+
+
+  public static sortOrder: number = 1;
+
+  public intern : Intern = {
+    name: 'Aubert',
+    firstname: 'Jean-Luc',
+    email: 'jla.webprojet@gmail.com',
+    phoneNumber: ' +(33)6 52 54 52 55'
+  }
+
+  public interns: Intern[] = [
+    this.intern,
+    {
+      name: 'Pina',
+      firstname: 'Monica',
+      email: 'ola@hotmail.com',
+      phoneNumber: '+(33) 00 22 11 55 54'
+    },
+    {
+      name: 'Castanie',
+      firstname: 'Piotr',
+      email: 'pedro@hotmail.com',
+      phoneNumber: '+(33) 51 47 54 0025'
+    },
+    {
+      name: 'jeagr',
+      firstname: 'Jean',
+      email: 'jeanneau@hotmail.com',
+      phoneNumber: '+(33) 00 00 0 00 25'
+    },
+  ]
+
+
+
+  public onDelete (intern: Intern): void {
+    this.interns.splice(
+      this.interns.indexOf(intern),
+      1
+    );
+  }
+
+
+ //trier par ordre croissant
+  public sortByName(): void {
+    Logger.info(`Before sort, sortOder is ${InternTableComponent.sortOrder}`)
+    this.interns.sort(
+      InternTableComponent.sortName
+    );
+    InternTableComponent.sortOrder = InternTableComponent.sortOrder * -1;
+    console.log(`After sort, sortOder is ${InternTableComponent.sortOrder}`)
+  }
+
+
+  //Static
+  private static sortName(intern1: Intern, intern2: Intern): number {
+    if (intern1.name > intern2.name){
+      return 1 * InternTableComponent.sortOrder;
+    } else if (intern1.name < intern2.name) {
+      return -1 * InternTableComponent.sortOrder;
+    } else {
+      return 0 * InternTableComponent.sortOrder;
+    }
+  }
+
+
+
+
+
+
+
+
+
+
 
 }
