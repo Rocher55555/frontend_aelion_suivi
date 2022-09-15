97483e0c29c0214de35701f88950bb4097ee0b72
pia <pia.andreotti@gmail.com>
2022-09-15 13:56:09 +0200

    UPDATE : stack from form to service

--- src/app/user/components/user-signin/user-signin.component.ts
+++ src/app/user/components/user-signin/user-signin.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-user-signin',
@@ -7,9 +9,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class UserSigninComponent implements OnInit {
 
-  constructor() { }
+  public signinForm!: FormGroup;
+
+  constructor(
+    private formBuilder: FormBuilder,
+    //injection du service
+    private userService: UserService
+  ) { }
 
   ngOnInit(): void {
+    this.signinForm = this.formBuilder.group({
+      login: [
+        '',  //Default value of login : ''
+        Validators.required
+      ],
+      pass: [
+        '',
+        Validators.required
+      ]
+    });
+  }
+/* appel la methode signin() de userservicepass lui les info saisies par l'user*/
+  public onSubmit(): void {
+    this.userService.signin(this.signinForm.value);
   }
 
 }
--- src/app/user/services/user.service.ts
+++ src/app/user/services/user.service.ts
@@ -1,9 +1,79 @@
 import { Injectable } from '@angular/core';
+import { Logger } from 'src/app/core/helpers/logger';
+import { UserModel } from '../models/user-model';
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class UserService {
 
+  private users: any[] = [
+    {
+      login: 'bond',
+      pass: '007'
+    },
+    {
+      login: 'casper',
+      pass: 'ghost'
+    }
+  ];
+
+  private user: UserModel | null = null;   // on part du principe que par default il set null
+
   constructor() { }
+
+  /**
+   *
+   * @param credentials From signinForm (login and password user entered)
+   * credetials : {login : 'toto', pass: 'titi'}
+   */
+  public signin(credentials: any): void {
+    Logger.info(JSON.stringify(credentials));
+    // Yassine : approche 1 boucle et comparaison
+    for (const inUser of this.users) {
+      if(inUser.login === credentials.login && inUser.pass === credentials.pass) {
+        //user was found
+        this.user = new UserModel()                      //
+        this.user.setLogin(credentials.login);
+        this.user.setToken(credentials.login + 'crypto.validité');
+        break;
+      }
+    }
+
+    //soit this.user est une instance de UserModel soit il est null
+  }
+
+
+
+
+
+    // Jean_Luc : find
+    /*
+    const foundUser: any = this.users.find(
+      (inUser: any) => inUser.login === credentials.login && inUser.pass ===credentials.pass
+      );
+    if (foundUser) {
+      this.user = new UserModel()
+      this.user.setLogin(credentials.login);
+      this.user.setToken(credentials.login + 'xxxxx.yyyyyy');
+    }
+    */
+
+
+
+  public signout(): void {
+    this.user = null;
+  }
+
+  /**
+   *
+   * @returns  Yes or No a user was authenticated
+   */
+
+  public isAuthenticated(): boolean {
+    return this.user !== null;
+  }
+
+
 }
