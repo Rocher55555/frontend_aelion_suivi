724f8928e12ba6ccf23ad70c3f676a8bd8329ef6
pia <pia.andreotti@gmail.com>
2022-09-15 14:55:53 +0200

    ADD a new snackbar in Service

---
+++ src/app/core/services/crud-snackbar.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@angular/core';
+import { MatSnackBar } from '@angular/material/snack-bar';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class CrudSnackbarService {
+
+  private message: string;
+  private action?: string;
+
+  private static duration: number = 3;
+
+  constructor(private snackBar: MatSnackBar) {
+    this.message = 'message';
+    this.action = 'action';
+  }
+
+
+  public config(message: string, action?: string) {
+    this.message = message;
+    this.action = action;
+  }
+
+  public open(): void {
+    this.snackBar.open(
+      this.message,
+      this.action,
+      {
+        duration: CrudSnackbarService.duration * 1000
+      }
+    )
+  }
+}
