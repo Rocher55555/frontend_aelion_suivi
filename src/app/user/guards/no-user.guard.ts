1fdee14fedfd58c240e1118fa931a1e411b62ca0
pia <pia.andreotti@gmail.com>
2022-09-16 11:10:08 +0200

    ADD UserService dans le constructor

---
+++ src/app/user/guards/no-user.guard.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
+import { UserService } from '../services/user.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+
+export class NoUserGuard implements CanActivate {
+
+  public constructor(
+    private userService: UserService,
+    private router: Router
+  ){}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+
+    if(this.userService.isAuthenticated()){
+      return true
+    }
+
+    this.router.navigate(['/','signin']);
+    return false;
+  }
+
+}
