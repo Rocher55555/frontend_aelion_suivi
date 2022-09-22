bc0b0e240ebd9b50ea4cd2d20f6e397a2ee1a695
pia <pia.andreotti@gmail.com>
2022-09-22 14:16:28 +0200

    UPDATE poe model : add Expose and type  decoration before each attributs

--- src/app/core/models/poe.ts
+++ src/app/core/models/poe.ts
@@ -1,7 +1,19 @@
+import { Expose, plainToInstance, Type } from "class-transformer";
+import 'reflect-metadata';
+
+// @Expose() => indique que cet attribut est concerné par la “deserialization”
+// @Type() => définit une fonction de “mapping” permettant de convertir la donnée “entrante” vers le type souhaité (ici Date)
+
 
 export class POE {
+  @Expose()
   public id?: number;
+  @Expose()
   public title!: string;
+  @Expose()
+  @Type(() => Date)
   public beginningDate?: Date;
+  @Expose()
+  @Type(() => Date)
   public endDate? : Date;
 }
