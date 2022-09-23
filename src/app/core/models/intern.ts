79936284f5cc424cad40dbaa6b6e4eec1cb72b9d
pia <pia.andreotti@gmail.com>
2022-09-23 11:31:27 +0200

    UPDATE: adding "deserialize" methode for interns, adding @Expose and @Type

--- src/app/core/models/intern.ts
+++ src/app/core/models/intern.ts
@@ -1,9 +1,30 @@
+import { Expose, plainToInstance, Type } from "class-transformer";
+
 export class Intern {
+  @Expose()
   public id?: number;
+  @Expose()
   public name!: string;
+  @Expose()
   public firstname?: string;
+  @Expose()
   public phoneNumber?: string;
+  @Expose()
+  @Type(() => Date)
   public email?: string;
+  @Expose()
+  @Type(() => Date)
   public birthDate?: Date;
+  @Expose()
   public address?: string;
+
+
+
+// deseriazation
+public deserialize(plainIntern: any): Intern {
+  const asClass: Intern = plainToInstance(Intern, plainIntern, {excludeExtraneousValues: true});
+  return asClass;
+  }
+
+
 }
