4c43f3d027498374cbabb24dfd5b82519c331043
pia <pia.andreotti@gmail.com>
2022-09-27 17:12:54 +0200

    CREATION AND UPDATE internFormBulder

--- src/app/intern/builder/intern-form-builder.ts
+++ src/app/intern/builder/intern-form-builder.ts
@@ -1,28 +1,57 @@
 import { Form, FormBuilder, FormControl, FormGroup, Validators } from "@angular/forms";
+import * as moment from "moment";
+import { map, Observable, take } from "rxjs";
 import { Logger } from "src/app/core/helpers/logger";
+import { Intern } from "src/app/core/models/intern";
+import { POE } from "src/app/core/models/poe";
+import { POEService } from "src/app/core/services/poe.service";
 import { DateValidator } from "src/app/core/validators/date-validator";
 import { EmailExistsValidatorService } from "src/app/core/validators/email-exists-validator.service";
 
 export class InternFormBuilder {
   private form: FormGroup | null = null;
   private addPoes : boolean = false;
+  private _poes: POE[] | null = null
+  private _intern: Intern= new Intern();     // the intern we want to manage (empty Model first)
+
 
 
   public constructor (
-    private formBuilder: FormBuilder// composition : class a
+    private formBuilder: FormBuilder,// composition : class a
+    private poeService: POEService
     ) {
+      this._intern.name = 'Andreotti';
+      this._intern.firstname = 'Pio'
+      this._intern.birthDate = new Date ('01/08/2020');
       this._buildForm();
     }
+
+
+
+
+
 
     //je vais en avoir besoin ailleurs donc GETTER
     public get internForm(): FormGroup {
       return this.form!;
     }
 
-    public toggleAddPoes():void {
-      this.addPoes = true;
-      const poesControl: FormControl = new FormControl ('', Validators.required);
-      this.form?.addControl('poes', poesControl);
+    public toggleAddPoes(): Observable<POE[]> {
+      return this.poeService.findAll()                        // retour tout le reste !
+      .pipe(
+        take(1),
+        map((poes: POE[]) => {
+        this.addPoes = true;
+        this._poes = poes;
+        const poesControl: FormControl = new FormControl ('', Validators.required);
+        this.form?.addControl('poes', poesControl);
+        return poes
+        })
+      )
+    }
+
+    public get poes(): POE[] | null {
+      return this._poes;
     }
 
   private _buildForm() : void {
@@ -32,7 +61,7 @@ export class InternFormBuilder {
    this.form = this.formBuilder.group(
     {
       name: [  // controls => control piloter par ele champ de formaulaire
-        '', //Defaut value for the fied control
+        this._intern.name, //Defaut value for the fied control
         Validators.compose(
           [
             Validators.required,
@@ -40,13 +69,13 @@ export class InternFormBuilder {
           ]
         )
       ], firstname: [
-        '',
+        this._intern.firstname,
         [
           Validators.required,    // nécessaire car il existe un pipe et directive pour la boule INITIAL
           Validators.minLength(2)
         ]
       ], email: [  // magie.tacher@bigben.co.uk
-        '',
+        this._intern.email,
         Validators.compose(
           [
             Validators.required,
@@ -57,9 +86,9 @@ export class InternFormBuilder {
           EmailExistsValidatorService.emailExists
           //this.emailExistsValidator.alreadyExists.bind(this.emailExistsValidator)
       ], phoneNumber: [
-        '',
+        this._intern.phoneNumber,
       ], birthDate: [
-        '',
+       this._intern.birthDate ? moment(this._intern.birthDate).format('YYYY-MM-DD') : '',
         [
           Validators.required,
           DateValidator.dateNotLessThan
