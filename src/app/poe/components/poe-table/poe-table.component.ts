846198467230ab7ba9ea7f7e6ec4f89768673c78
pia <pia.andreotti@gmail.com>
2022-10-17 15:27:11 +0200

    UPDATE : add delete methode

--- src/app/poe/components/poe-table/poe-table.component.ts
+++ src/app/poe/components/poe-table/poe-table.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { POEService } from './../../../core/services/poe.service';
 import { POE } from 'src/app/core/models/poe';
 import { take } from 'rxjs';
+import { HttpResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-poe-table',
@@ -25,6 +26,24 @@ export class POETableComponent implements OnInit {
     .subscribe((poes: POE[]) => {
       this.poes = poes;
       })
+  }
+/**
+ * return 204 if ok, 404 if the POE is not found or a 401 status
+ * @param poe
+ */
+  public delete (poe:POE):void {
+    console.log('Hey delete fonctionne')
+    this.poeService.delete(poe)
+    .pipe(
+      take(1)
+    ).subscribe((Response:HttpResponse<any>) => {
+      if(Response.status === 204){
+        this.poes.splice(
+          this.poes.findIndex((obj: POE) => obj.id === poe.id),
+          1
+        );
+      }
+    })
   }
 
 
