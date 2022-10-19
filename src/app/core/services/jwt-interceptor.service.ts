25539f6d279701d7ed2b146453c902fc2d2617b6
Rocher55555 <albertdushmoll@gmail.com>
2022-10-19 11:59:31 +0200

    UPDATE Fixed CORS request issue on address autocomplete

--- src/app/core/services/jwt-interceptor.service.ts
+++ src/app/core/services/jwt-interceptor.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserModel } from 'src/app/user/models/user-model';
 import { UserService } from 'src/app/user/services/user.service';
+import { environment } from 'src/environments/environment';
 import { Logger } from '../helpers/logger';
 
 @Injectable({
@@ -17,16 +18,18 @@ export class JwtInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
     Logger.info(`Intercept a HTTP Request from Interceptor`);
     //We have to add an Authorization header to the HTTP Reques if a user was authenticated
-    if(this.userService.isAuthenticated()){
-      Logger.info(`JwtInterceptorService-20:: A user was authenticated`);
-      //Faudrait que je recpere le token
-      const authenticatedUser: UserModel | null = this.userService.getUser();
-      //Cloner la requete en lui ajoutant 'en-tête souhaitée
-      if (authenticatedUser !== null){
-        if(!req.headers.has('Authorization')){
-          req = req.clone(
-            {headers:req.headers.set('Authorization', `Bearer ${authenticatedUser.getToken()}`)}
-          );
+    if (this.userService.isAuthenticated()) {
+      if (req.url.includes(environment.apiRoot)) {
+        Logger.info(`JwtInterceptorService-20:: A user was authenticated`);
+        //Faudrait que je recpere le token
+        const authenticatedUser: UserModel | null = this.userService.getUser();
+        //Cloner la requete en lui ajoutant 'en-tête souhaitée
+        if (authenticatedUser !== null) {
+          if (!req.headers.has('Authorization')) {
+            req = req.clone(
+              { headers: req.headers.set('Authorization', `Bearer ${authenticatedUser.getToken()}`) }
+            );
+          }
         }
       }
     }
